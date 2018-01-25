41ee442acd592ad860a5b001d8d7b677d3535e3e
robert <robert@consulta.com.mx>
2018-01-25 17:55:23 -0600

    Vistas y rutas

--- web_programacion/config/routes.rb
+++ web_programacion/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 	root 'index#muestra_presentacion'
 	#Contenido de la página
-	get '/presentacion' => 'index#muestra_presentacion'
-	get '/profesores' => 'index#muestra_profesores'
-	get '/evaluacion' => 'index#muestra_evaluacion'
-	get '/material' => 'index#muestra_material'
-	get '/eval_tareas' => 'index#muestra_eval_tareas'
-	get '/archivos_tareas' => 'index#muestra_archivos_tareas'
+	get '/presentacion' => 'index#presentacion'
+	get '/profesores' => 'index#profesores'
+	get '/temario' => 'index#temario'
+	get '/evaluacion' => 'index#evaluacion'
+	get '/material' => 'index#material'
+	get '/eval_tareas' => 'index#eval_tareas'
+	get '/archivos_tareas' => 'index#archivos_tareas'
+
+	get '/descargar' => 'index#descargar'
+	get '/subir_archivo' => 'index#material'
 end
